4ce412a45e1925816d9d2b51bd3eb7f33c252f74
Dmytro Nefyodov <frolovvtor@gmail.com>
2024-01-12 00:06:52 +0200

    feat: [DEPT-0000] initial project

---
+++ wagtail_dept/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zu$-1wzrd5b81-5m725!lm=!#2*_3k#2sf8%dl@e+&$d$ut(o-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
