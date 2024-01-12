8d90329b79d1ab94d4682f202a6f701db4140eef
Dmytro Nefyodov <frolovvtor@gmail.com>
2024-01-12 23:58:40 +0200

    feat: [DEPT-0001] add blogs apps

--- book_repository/models.py
+++ book_repository/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+
+
+class AuthorPage(Page):
+    name = RichTextField()
+    description = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('description'),
+        InlinePanel('books', heading="Books", label="Book"),
+    ]
+
+    class Meta:
+        verbose_name = "Author Page"
+        verbose_name_plural = "Author Pages"
+
+class BookPage(Orderable):
+    author = ParentalKey(AuthorPage, on_delete=models.CASCADE, related_name='books')
+    name = RichTextField()
+    description = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('description'),
+    ]
